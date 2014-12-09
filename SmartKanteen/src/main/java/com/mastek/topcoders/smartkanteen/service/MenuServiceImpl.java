07956e99acf279ab54e3c776c06f0ec001be66a2
tarulyadav <tarul.yadav@mastek.com>
2014-12-09 14:04:48 +0530

    Updated MenuService and MenuServiceImpl added Test.java

--- SmartKanteen/src/main/java/com/mastek/topcoders/smartkanteen/service/MenuServiceImpl.java
+++ SmartKanteen/src/main/java/com/mastek/topcoders/smartkanteen/service/MenuServiceImpl.java
@@ -4,8 +4,6 @@ import java.math.BigDecimal;
 import java.util.Date;
 import java.util.List;
 
-import org.hibernate.HibernateException;
-
 import com.mastek.topcoders.smartkanteen.bean.Caterer;
 import com.mastek.topcoders.smartkanteen.bean.DailyMenu;
 import com.mastek.topcoders.smartkanteen.bean.Menu;
@@ -31,17 +29,29 @@ public class MenuServiceImpl implements MenuService
 		return dao.addItem(menuMaster);
 	}
 
-	public void updateItemInMenuMaster(Integer itemId, String itemName, String description, BigDecimal price,
-			Integer prepTime)
+	public Integer addItemInMenuMaster(Menu menuMaster, Caterer caterer)
 	{
-		if (itemId == null || itemId == 0)
-		{
-			System.out.println("Item not found");
-		}
-		
 		MenuDAO dao = new MenuDAO();
+		dao.addItemInMenuMaster(menuMaster, caterer);
+		return null;
+	}
+	
+	/* Added by @vaibhav
+	 * testing Post Operation on RS
+	 */
+	@Override
+	public void addItemMenuMaster(String itemName, String description,
+			BigDecimal price, Integer prepTime,Integer category) {
+		// TODO Auto-generated method stub
+		MenuDAO dao = new MenuDAO();
+		//dao.addItemInMenuMaster(itemName,  description,
+			//	 price, prepTime,category);
 		
-		Menu menuMaster = dao.getItem(itemId);
+		//MenuDAO dao = new MenuDAO();
+		
+		//Menu menuMaster = dao.getItem(itemId);
+		
+		Menu menuMaster =new Menu();
 		
 		if(itemName!=null)
 		{
@@ -63,8 +73,54 @@ public class MenuServiceImpl implements MenuService
 			menuMaster.setPrepTime(prepTime);
 		}
 		
+		if(category!=null)
+		{
+			//menuMaster.se(prepTime);
+		}
 		
-		dao.updateItem(menuMaster);
+		dao.addItem(menuMaster);
+	}
+
+	public void updateItemInMenuMaster(Integer itemId, String itemName, String description, BigDecimal price,
+			Integer prepTime)
+	{
+		if (itemId == null || itemId == 0)
+		{
+			System.out.println("Item not found");
+		}
+
+		MenuDAO dao = new MenuDAO();
+
+		Menu menuMaster = dao.getItem(itemId);
+
+		if (menuMaster != null)
+		{
+			if (itemName != null)
+			{
+				menuMaster.setItemName(itemName);
+			}
+
+			if (description != null)
+			{
+				menuMaster.setDescription(description);
+			}
+
+			if (price != null)
+			{
+				menuMaster.setPrice(price);
+			}
+
+			if (prepTime != null)
+			{
+				menuMaster.setPrepTime(prepTime);
+			}
+
+			dao.updateItem(menuMaster);
+		}
+		else
+		{
+			System.out.println("Menu item not found!!");
+		}
 	}
 
 	public void deleteItemFromMenuMaster(Integer itemId)
@@ -73,75 +129,77 @@ public class MenuServiceImpl implements MenuService
 		dao.deleteItem(itemId);
 	}
 
-	public void deleteAllItems()
+	public void deleteAllItemsFromMenuMaster()
 	{
 		MenuDAO dao = new MenuDAO();
-		dao.deleteAllItems();
-	}
-
-	public Integer addItemInMenuMaster(Menu menuMaster, Caterer caterer)
-	{
-		MenuDAO dao = new MenuDAO();
-		dao.addItemInMenuMaster(menuMaster, caterer);
-		return null;
+		dao.deleteAllItemsFromMenuMaster();
 	}
 
 	public Caterer getCaterer(Integer catererId)
 	{
-		// TODO Auto-generated method stub
-		return null;
+		MenuDAO dao = new MenuDAO();
+		Caterer caterer = dao.getCaterer(catererId);
+		return caterer;
 	}
 
 	public List<Caterer> getCaterers()
 	{
-		// TODO Auto-generated method stub
-		return null;
+		MenuDAO dao = new MenuDAO();
+		List<Caterer> catererList = dao.getCaterers();
+		return catererList;
 	}
 
+	public Integer addCaterer(Caterer caterer)
+	{
+		MenuDAO dao = new MenuDAO();
+		Integer result = dao.addCaterer(caterer);
+		return result;
+	}
+
+	public Boolean updateCaterer(Integer catererId, String catererName)
+	{
+		boolean result = false;
+
+		if (catererId == 0 || catererId == null)
+		{
+			System.out.println("CatererId not found");
+		}
+
+		MenuDAO dao = new MenuDAO();
+		Caterer caterer = dao.getCaterer(catererId);
+
+		if (caterer != null)
+		{
+			if (catererName != null)
+			{
+				caterer.setCatererName(catererName);
+			}
+			result = dao.updateCaterer(caterer);
+		}
+		else
+		{
+			System.out.println("Caterer not found!!");
+		}
+		return result;
+	}
+
+	@Override
+	public Boolean deleteCaterer(Integer catererId)
+	{
+		MenuDAO dao = new MenuDAO();
+		boolean result = dao.deleteCaterer(catererId);
+		return result;
+	}
+
+	public void deleteAllCaterers()
+	{
+		MenuDAO dao = new MenuDAO();
+		dao.deleteAllCaterers();
+	}
+	
 	public DailyMenu getDailyMenu(Date menuDate, Integer catererId)
 	{
 		// TODO Auto-generated method stub
 		return null;
-	}
-	
-	public static void main(String[] args)
-	{
-		try
-		{
-			MenuServiceImpl service = new MenuServiceImpl();
-
-			System.out.println("Menu List");
-
-			service.deleteAllItems();
-			System.out.println(service.getMenuMaster());
-
-			Menu menuMaster = new Menu();
-			menuMaster.setItemId(26);
-			menuMaster.setItemName("Thai Rice");
-			menuMaster.setDescription("Chinese preparation");
-			menuMaster.setPrepTime(5);
-			menuMaster.setPrice(new BigDecimal(40));
-			service.addItemInMenuMaster(menuMaster);
-
-			System.out.println("After Adding Item");
-			System.out.println(service.getMenuMaster());
-
-			service.updateItemInMenuMaster(26, "Five Rice", null, null, null);
-			
-			System.out.println("After Updating Item");
-			System.out.println(service.getMenuMaster());
-			
-			service.deleteItemFromMenuMaster(26);
-
-			System.out.println("After Deleting Item");
-			System.out.println(service.getMenuMaster());
-
-			// System.out.println(service.getMenuMasterByCaterer(1));
-
-		}
-		catch (HibernateException e)
-		{
-			e.printStackTrace();
-		}
 	}
 }
