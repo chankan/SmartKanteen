9ed79217396c5058f590c35d27b879d42624baba
Rahul <rahul.panchal@mastek.com>
2014-12-09 17:52:37 +0530

    Adding DailyMenuMapping java and hbm.xml file

---
+++ SmartKanteen/src/main/java/com/mastek/topcoders/smartkanteen/bean/DailyMenuMapping.java
@@ -0,0 +1,45 @@
+package com.mastek.topcoders.smartkanteen.bean;
+
+public class DailyMenuMapping
+{
+	private int dailyMenuMappingId;
+	private Menu menu;
+	private int dailyMenuId;
+
+	public int getDailyMenuMappingId()
+	{
+		return dailyMenuMappingId;
+	}
+
+	public void setDailyMenuMappingId(int dailyMenuMappingId)
+	{
+		this.dailyMenuMappingId = dailyMenuMappingId;
+	}
+
+	public Menu getMenu()
+	{
+		return menu;
+	}
+
+	public void setMenu(Menu menu)
+	{
+		this.menu = menu;
+	}
+
+	public int getDailyMenuId()
+	{
+		return dailyMenuId;
+	}
+
+	public void setDailyMenuId(int dailyMenuId)
+	{
+		this.dailyMenuId = dailyMenuId;
+	}
+
+	@Override
+	public String toString()
+	{
+		return "DailyMenuMapping [menu=" + menu + ", dailyMenuId=" + dailyMenuId + "]";
+	}
+
+}
