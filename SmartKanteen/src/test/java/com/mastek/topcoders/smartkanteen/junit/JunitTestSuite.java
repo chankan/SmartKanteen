287f0fd85d87bb4e97d831539c0410d9e466b6b0
Rahul <rahul.panchal@mastek.com>
2014-12-22 12:28:16 +0530

    Junit test cases

--- SmartKanteen/src/test/java/com/mastek/topcoders/smartkanteen/junit/JunitTestSuite.java
+++ SmartKanteen/src/test/java/com/mastek/topcoders/smartkanteen/junit/JunitTestSuite.java
@@ -4,12 +4,12 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import com.mastek.topcoders.smartkanteen.bean.Caterer;
+import com.mastek.topcoders.smartkanteen.bean.Menu;
 import com.mastek.topcoders.smartkanteen.service.MenuServiceImpl;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({
-	TestMenuService.class
-})
+@Suite.SuiteClasses({ CatererTestCase.class, MenuTestCase.class, DailyMenuTestCase.class })
 public class JunitTestSuite
 {
 
--- SmartKanteen/src/test/java/com/mastek/topcoders/smartkanteen/junit/Test.java
+++ SmartKanteen/src/test/java/com/mastek/topcoders/smartkanteen/junit/Test.java
@@ -4,18 +4,15 @@ import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 
-
 public class Test
 {
-
 	public static void main(String[] args)
 	{
-		Result  result= JUnitCore.runClasses(JunitTestSuite.class);
-		for (Failure failure :result.getFailures())
+		Result result = JUnitCore.runClasses(JunitTestSuite.class);
+		for (Failure failure : result.getFailures())
 		{
 			System.out.println(failure.toString());
 		}
-       System.out.println(result.wasSuccessful());
+		System.out.println(result.wasSuccessful());
 	}
-
 }
